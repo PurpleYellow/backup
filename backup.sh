96fd22d9d0563cae51778bb1caeaf49363b77030
Arnoldas Grigutis <a.grigutis@paysera.com>
2017-11-19 19:06:48 +0200

    Added backup.sh

---
+++ backup.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+HOMEDIR=/home/arnoldas
+BACKUP_NAME=backup-$(date -u +"%Y-%m-%d-%H-%M-%S-%Z")
+
+# Ensure the passphrase file is set up
+PWD_PATH=$HOMEDIR/.backup_password
+if ! [ -f $PWD_PATH ]; then
+    echo "Exiting: create a file $PWD_PATH with a password inside which will be used to
+encrypt the archived backup, set its permissions to 600."
+    exit
+fi;
+if [ $(stat -c %a "$PWD_PATH") != 600 ]; then
+    echo "Exiting: set up permissions 600 the password file."$PWD_PATH
+    exit
+fi;
+
+# Ensure the backup root dirs set up
+BACKUP_ROOT_DIR=$HOMEDIR/backups
+if ! [ -d "$BACKUP_ROOT_DIR" ]; then
+    mkdir "$BACKUP_ROOT_DIR"
+fi
+
+# Archive
+BACKUP_ARCHIVED_FILE="$BACKUP_ROOT_DIR/$BACKUP_NAME.tar.gz"
+tar czf "$BACKUP_ARCHIVED_FILE" $HOMEDIR/.ssh $HOMEDIR/Documents $HOMEDIR/bin $HOMEDIR/Desktop $HOMEDIR/.local/share/gnote
+
+# Encrypt the archive
+if ! [ -x "$(command -v gpg)" ]; then
+    echo "Warning: not encrypting file because gpg is not installed."
+else
+    gpg --symmetric --cipher-algo AES256 --passphrase $(cat "$PWD_PATH") "$BACKUP_ARCHIVED_FILE" 
+fi
+
+# Delete old files
+if ! [ -x "$(command -v srm)" ]; then
+    echo "Warning: using insecure delete via rm. Install srm."
+    rm -r "$BACKUP_ARCHIVED_FILE"
+else
+    srm -r "$BACKUP_ARCHIVED_FILE"
+fi
+
+echo "Done"
+
